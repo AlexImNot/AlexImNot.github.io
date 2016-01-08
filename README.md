1950410a9c0ef907ac852c5ae7d99cc29c0ce254
Alexander Samuelsen-Manso <alexander@samuels1.no>
2016-01-08 22:49:48 +0100

    Upated to support HTML
    1.0.2

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ You may be allowed to use my code from website
 AS LONG as you give credits back to me.
 
 # Update:
-* Setting up website
+* Updated JS
+* PhP -> HTML5
+* Fixing CSS
--- assets/extra/meta.php
+++
@@ -1,9 +0,0 @@
-<?php include "steam.php" ?>
-
-<meta content="<?php echo $link ?>" property="og:url" />
-<meta content="website" property="og:type" />
-<meta name="author" content="Alexander Samuelsen-Manso">
-<meta name="description" content="Hi, i'm a Web-Developer, take contact with me if you wish to speak with me about something!">
-<meta name="keywords" content="Alexander, Samuelsen-Manso, HTML5, CSS3, PhP, JavaScript, Js, Windows, Linux, FTP, Coder, Web-Developer, Web, Developer">
-<meta content="<?php echo $str_avatar; ?>" property="og:image" />
-<link rel="image_src" href="<?php echo $str_avatar; ?>">
